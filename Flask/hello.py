950f91ba65a9872ad0c7312dc422fcf4a6007c8f
lakshas <slakshan286@gmail.com>
2024-11-15 15:00:22 +0530

    Decorator Fn

--- Flask/hello.py
+++ Flask/hello.py
@@ -1,8 +1,16 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+print(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+# Decorator Fn
+@app.route("/bye")
+def bye():
+    return "Bye Nibba..!!"
+
+if __name__ == "__main__":
+    app.run()
