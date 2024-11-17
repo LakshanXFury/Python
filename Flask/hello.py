824fe54c1ada57d5e1020ada31b683050d4c6f74
lakshas <slakshan286@gmail.com>
2024-11-17 20:19:10 +0530

    Decorator Fn

--- Flask/hello.py
+++ Flask/hello.py
@@ -5,12 +5,21 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return ('<h1 style="text-align: center; color:Tomato;">AOT</h1>'
+            '<p> <b> Attack On Titan </b> </p>'
+            '<img src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGxybnZ2amlhY2d1eTRtNHVmMWQwbzEyeDY0YXhzNWNrMzdoa'
+            '2I3byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0Iyoy5VglR6QKJiw/giphy.gif">')
 
 # Decorator Fn
 @app.route("/bye")
 def bye():
     return "Bye Nibba..!!"
 
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there {name}, you are {number} years old..!!!"
+
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
