965f5b1578fc5a4f3754a15c017f9ca408eb865c
lakshas <slakshan286@gmail.com>
2024-11-27 20:20:25 +0530

    BootStrap Blog website

--- upgraded blog/main.py
+++ upgraded blog/main.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+response = requests.get("https://api.npoint.io/756b02afb8f0d5ae25e4")
+post_response = response.json()
 
 
 #Decorator
 @app.route('/')
 def home():
-    return render_template("index.html")
+    print(post_response)
+
+    return render_template("index.html", posts=post_response)
 
 @app.route('/about')
 def about():
@@ -18,6 +23,16 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route('/blog/<int:index>')
+def post_body(index):
+    print(index)
+    requested_post = None
+    for blog_post in post_response:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+            print(f"This is the blog posst chosen{requested_post}")
+
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
