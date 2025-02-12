acbd31e22872db751c63eabd28018d563ee5b231
lakshas <slakshan286@gmail.com>
2025-02-12 13:46:35 +0530

    Protfolio website Project

--- Projects/porfolio_website/main.py
+++ Projects/porfolio_website/main.py
@@ -10,12 +10,9 @@ bootstrap = Bootstrap5(app)
 def home():
     return render_template("index.html")
 
-
-
-
-
-
-
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
