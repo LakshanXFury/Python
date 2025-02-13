3c679f1d5ae4f2374e672a4b737ad164d8d4b6c6
lakshas <slakshan286@gmail.com>
2025-02-13 20:49:46 +0530

    Protfolio website Project

--- Projects/porfolio_website/main.py
+++ Projects/porfolio_website/main.py
@@ -14,5 +14,13 @@ def home():
 def portfolio():
     return render_template("portfolio.html")
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
