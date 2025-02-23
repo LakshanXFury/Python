f810a8174aa5b1ef84862a03f91c1a487e9239b6
lakshas <slakshan286@gmail.com>
2025-02-23 19:53:08 +0530

    Typing spped app using Tkinter

---
+++ Projects/typing_speed_app.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+
+# Initialize Tkinter window
+window = Tk()
+window.title("Typing speed test app")
+window.minsize(width=600, height=500)
+window.config(padx=20, pady=20)
+
+
+
+
+
+
+
+# Run Tkinter
+window.mainloop()
