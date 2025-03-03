a98f78b9ed88318c5c1cbbae7d12ba9ca742bba9
lakshas <slakshan286@gmail.com>
2025-03-03 18:28:24 +0530

    Breakout game using Turtle

--- Projects/Breakout_Game/main.py
+++ Projects/Breakout_Game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from bar import Bar
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,15 +11,27 @@ screen.tracer(0)  # Zero Dotted lines
 
 bar = Bar((-100, -250))  # Move to bottom (adjust based on window size)
 
+ball = Ball()
 
-screen.listen()
+
 screen.onkey(bar.go_left, key="Left")
 screen.onkey(bar.go_right, key="Right")
 
 # Main game loop
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.movespeed)
     screen.update()  # Refresh screen continuously
+    ball.move()
+
+    # Detect the collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:  # upper and lower
+        # needs to bounce
+        ball.bounce_y()
+    
+    if ball.xcor() > 280:
+        ball.bounce_y()
+
 
 
 
