979c1ca76331af7245aa34f2cf7e329fd6a6f389
lakshas <slakshan286@gmail.com>
2025-05-25 20:31:43 +0530

    2nd largest in the array

---
+++ coding_practice/array/2nd_largest.py
@@ -0,0 +1,22 @@
+def find_second_largest(arr):
+    if len(arr) < 2:
+        return "Array must have at least two elements."
+
+    largest = second_largest = float('-inf')  # means negative infinity.
+
+    for num in arr:
+        if num > largest:
+            second_largest = largest
+            largest = num
+        elif num > second_largest and num != largest:
+            second_largest = num
+
+    if second_largest == float('-inf'):
+        return "There is no second largest (all elements may be equal)."
+
+    return second_largest
+
+
+# Example usage
+arr = [10, 5, 20, 8, 20]
+print("Second largest number is:", find_second_largest(arr))
