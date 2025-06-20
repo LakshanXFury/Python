e8f0bcc4eca43abce94a1f25e9c31b1cce4ef676
lakshas <slakshan286@gmail.com>
2025-06-20 18:34:53 +0530

    Find the Factorial of a Number

---
+++ coding_practice/factorial.py
@@ -0,0 +1,27 @@
+# Find the Factorial of a Number
+def factorial_num(number):
+    value = 0
+    for i in range(int(number), 0, -1):
+        if value == 0:
+            value = i
+        else:
+            value = value * i
+
+    print(f"The factorial of the given number {number} is {value}")
+    return value
+
+
+print(factorial_num(5))
+factorial_num(6)
+
+# Using While loop
+
+# def fact(number):
+#     result = 1
+#     while number >= 1:
+#         result = result * number
+#         number = number - 1
+#     return result
+#
+#
+# print(fact(5))
