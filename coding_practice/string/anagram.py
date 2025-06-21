9643cff7904ab8f030bc3b500e94312ab716f342
lakshas <slakshan286@gmail.com>
2025-06-21 19:14:26 +0530

    Check if two strings are anagrams.

---
+++ coding_practice/string/anagram.py
@@ -0,0 +1,21 @@
+# Check if two strings are anagrams.
+"""
+A word or phrase that is made by arranging the letters of another word or phrase in a different order.
+Fried = Fired, Listen = Silent
+"""
+
+
+def anagram(first_word, second_word):
+    a = first_word.replace(" ", "").lower()
+    b = second_word.replace(" ", "").lower()
+
+    lista = sorted(list(a))
+    listb = sorted(list(b))
+
+    if lista == listb:
+        print("Your words are Anagram")
+    else:
+        print("Your words are not an Anagram")
+
+
+anagram("the eyes", "They see")
