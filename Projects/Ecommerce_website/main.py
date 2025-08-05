10d4f40e1c375c2be23d4068ee8d378827dbe8d3
lakshas <slakshan286@gmail.com>
2025-08-05 18:00:52 +0530

    E-commerce Website, The header part

--- Projects/Ecommerce_website/main.py
+++ Projects/Ecommerce_website/main.py
@@ -1,13 +1,28 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
 
 app = Flask(__name__)
 Bootstrap5(app)
 
 
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+
+# Connect to Database
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db?timeout=10'  #This sets a 10-second timeout, allowing SQLite to wait before raising a locked error.
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
 @app.route("/")
 def home():
-    pass
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
