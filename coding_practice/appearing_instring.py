1611d60b5ca0bd9d7da2456845bf0241cd0fba54
lakshas <slakshan286@gmail.com>
2025-09-01 20:28:08 +0530

    No of times the word appearing in the String

--- coding_practice/appearing_instring.py
+++ coding_practice/appearing_instring.py
@@ -1,18 +1,19 @@
 # Count the frequency of words appearing in a string Using Python
 
-#The little bird kept chirping, chirping, and chirping as it flew over the bright, blue sky, enjoying the fresh breeze and the warmth of the glowing, glowing sun.
-def appearing():
-    string = input("Enter a sentence: ").lower()
+# The little bird kept chirping, chirping, and chirping as it flew over the bright, blue sky, enjoying the fresh
+# breeze and the warmth of the glowing, glowing sun.
+def appearing(string):
     li = string.split()
     print(li)
     d = {}
 
     for i in li:
-        if i not in d.keys():
-            d[i] = 0
-        d[i] = d[i] + 1
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
     print(d)
 
 
-
-appearing()
+string_input = input("Enter a sentence: ").lower()
+appearing(string_input)
