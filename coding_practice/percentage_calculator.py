55bbf2290ef89b5812e99a6d331a6097b78d73d5
lakshas <slakshan286@gmail.com>
2025-10-14 18:32:04 +0530

    Percentage Calculator

---
+++ coding_practice/percentage_calculator.py
@@ -0,0 +1,7 @@
+def percentage(number: int, percent: int):
+    final_percentage = percent / 100 * number
+    print(f"{percent}% of {number} is :{final_percentage}")
+
+
+percentage(107, 6)
+percentage(120, 40)
