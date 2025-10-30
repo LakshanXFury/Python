93e9b10b584e9c4148952c6db143a6e19027c613
lakshas <slakshan286@gmail.com>
2025-10-30 14:53:29 +0530

    Tuples and list difference in python

---
+++ course/tuple&list.py
@@ -0,0 +1,9 @@
+mylist = ['apple', 'orange', 'banana']
+print('mylist before change: ', mylist)
+mylist[0] = 'not an apple'
+print('mylist after change: ', mylist)
+
+mytuple = ('apple', 'orange', 'banana')
+print('mytuple before change: ', mytuple)
+mytuple[0] = 'not an apple'
+print('mytuple after change: ', mytuple)
