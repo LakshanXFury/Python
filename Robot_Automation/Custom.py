be4dab0f22f2850486609959281143d16e41f175
lakshas <slakshan286@gmail.com>
2025-11-02 19:25:27 +0530

    Custome keyword using python in Robot

--- Robot_Automation/Custom.py
+++ Robot_Automation/Custom.py
@@ -4,3 +4,11 @@
 class Custom:
     def add(self, a, b):
         return int(a) + int(b)
+
+    def used_name(self, name):
+        """Prints the name (Robot keyword will be 'Used Name')"""
+        print(f"Working on the name {name}")
+
+    def full_name(self, first_name, last_name):
+        """Concatenate first and last name (Robot keyword 'Full Name')"""
+        return f"{first_name} {last_name}"
--- Robot_Automation/custom_keyword.robot
+++ Robot_Automation/custom_keyword.robot
@@ -2,8 +2,21 @@
 
 Library    Custom.py
 
+
+*** Variables ***
+${FIRST}    John
+${LAST}     Doew
+
+
 *** Test Cases ***
 Add Numbers
     ${result}=     add    2    3
     should be equal as integers    ${result}    5
     log    ${result}
+
+Test Used Name
+    Used Name    ${FIRST}
+
+Test Full Name
+    ${name}=    Full Name    ${FIRST}    ${LAST}
+    Log    Full name is: ${name}
