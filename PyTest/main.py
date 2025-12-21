e8e2d7e269b1b6314a4053f64a699b02da438fc5
lakshans <slakshan286@gmail.com>
2025-12-21 19:35:22 +0530

    Pytest exception raising using "pytest.raises"

--- PyTest/main.py
+++ PyTest/main.py
@@ -2,3 +2,15 @@ def add(a:int, b:int):
     return a+b
 
 print(add(5, 10))
+
+
+def get_weather(temp:int):
+    if temp > 20:
+        return "hot"
+    else:
+        return "cold"
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a/b
--- PyTest/test_main.py
+++ PyTest/test_main.py
@@ -1,7 +1,18 @@
-from main import add
+import pytest
+
+from main import add, get_weather, divide
 
 def test_add():
     assert add(2, 3) == 5, "2 + 3 should be 5"
 
 def test_add_fail():
     assert add(2, 3) == 10, "2 + 3 should be 5, if 10 it should fail"
+
+def test_get_weather():
+    assert get_weather(21) == "hot"
+
+# Raise Error
+def test_divide():
+    """pytest.raises is a context manager that lets you test whether your code raises a specific exception as expected."""
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(10,0)
