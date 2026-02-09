1cf166de33c31715d05a645bc05739129070e09e
lakshans <slakshan286@gmail.com>
2026-02-09 23:15:04 +0530

    2d list Matrix

---
+++ coding_practice/2d_matrix.py
@@ -0,0 +1,20 @@
+"""
+2D Matrix Problem
+"""
+
+li=[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+for j in range(4):
+    for ele in li:
+        print(ele[j],end="")
+
+
+
+"""
+What the code prints
+
+Column 0: 1  5  9
+Column 1: 2  6  10
+Column 2: 3  7  11
+Column 3: 4  8  12
+
+"""
