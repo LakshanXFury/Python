861f7e7539f2e555697d50c2ea2311991f7389b1
lakshans <slakshan286@gmail.com>
2026-04-26 20:18:45 +0530

    Open AI agent sdk using Asyncio

--- AI Agent/openai_agents_sdk.py
+++ AI Agent/openai_agents_sdk.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-
 agent = Agent(name="Jokester", instructions="You are a joke teller, who tells jokes", model="gpt-4o-mini")
 
 user_input = input(str("Enter the Topic in which you want the Joke to be asked for: "))
