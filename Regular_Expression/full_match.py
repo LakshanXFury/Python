a409044ecd079794defe032d21768254d937d44b
lakshans <slakshan286@gmail.com>
2026-09-24 23:42:07 +0530

    Regular Expression

--- Regular_Expression/full_match.py
+++ Regular_Expression/full_match.py
@@ -3,7 +3,7 @@ import re
 # Use (com|in|org) with | as OR to whitelist only specific extensions you want to allow.
 pattern = r"[a-zA-Z0-9]+@[a-zA-Z]+\.(com|in|org)"
 
-input_data = input(str("Enter the email address: "))
+input_data = input("Enter the email address: ")
 
 if re.fullmatch(pattern, input_data):
     print("Success")
